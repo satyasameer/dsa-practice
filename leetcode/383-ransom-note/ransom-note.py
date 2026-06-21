1f7d2951d152d2443fcc4e09007c41a8264a2c9b
satyasameer <19973069+satyasameer@users.noreply.github.com>
2026-06-21 17:18:52 +0200

    Time: 24 ms (41.84%) | Memory: 19.6 MB (33.36%) - LeetSync

--- leetcode/383-ransom-note/ransom-note.py
+++ leetcode/383-ransom-note/ransom-note.py
@@ -1,13 +1,21 @@
 class Solution:
     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
-        count_chars_m = {}
-        count_chars_r = {}
-        for char in ransomNote:
-            count_chars_r[char] = count_chars_r.get(char, 0) + 1
+        count = {}
         for char in magazine:
-            count_chars_m[char] = count_chars_m.get(char, 0) + 1
-        # return all(count_chars_m.get(char,-1) >= count_chars_r.get(char) for char in count_chars_r)
-        for char in count_chars_r:
-            if count_chars_m.get(char,-1) < count_chars_r.get(char):
+            count[char] = count.get(char, 0) + 1
+        for char in ransomNote:
+            if count.get(char, 0) == 0:
                 return False
+            count[char] -= 1
         return True
+        # count_chars_m = {}
+        # count_chars_r = {}
+        # for char in ransomNote:
+        #     count_chars_r[char] = count_chars_r.get(char, 0) + 1
+        # for char in magazine:
+        #     count_chars_m[char] = count_chars_m.get(char, 0) + 1
+        # # return all(count_chars_m.get(char,-1) >= count_chars_r.get(char) for char in count_chars_r)
+        # for char in count_chars_r:
+        #     if count_chars_m.get(char,-1) < count_chars_r.get(char):
+        #         return False
+        # return True
