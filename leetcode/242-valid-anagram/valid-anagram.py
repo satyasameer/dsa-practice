95fa53de25efeb2b854e5734182a87576b82df16
satyasameer <19973069+satyasameer@users.noreply.github.com>
2026-06-21 14:47:35 +0200

    Time: 11 ms (76.66%) | Memory: 19.3 MB (94.36%) - LeetSync

--- leetcode/242-valid-anagram/valid-anagram.py
+++ leetcode/242-valid-anagram/valid-anagram.py
@@ -2,10 +2,13 @@ class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
         if len(s) != len(t):
             return False
-        char_s_count = {}
-        char_t_count = {}
+        char_count = {}
+        # char_t_count = {}
         for char in s:
-            char_s_count[char] = char_s_count.get(char, 0) + 1
+            # char_s_count[char] = char_s_count.get(char, 0) + 1
+            char_count[char] = char_count.get(char, 0) + 1
         for char in t:
-            char_t_count[char] = char_t_count.get(char, 0) + 1
-        return char_s_count == char_t_count 
+            # char_t_count[char] = char_t_count.get(char, 0) + 1
+            char_count[char] = char_count.get(char, 0) - 1
+        return not any(char_count.values())
+        # return char_s_count == char_t_count 
