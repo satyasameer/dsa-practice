13bde7e3ba3574a122b0d2ca9a18b1f012ae3ba4
satyasameer <19973069+satyasameer@users.noreply.github.com>
2026-07-04 01:09:29 +0200

    Time: 35 ms (51.60%) | Memory: 21.9 MB (56.36%) - LeetSync

--- leetcode/560-subarray-sum-equals-k/subarray-sum-equals-k.py
+++ leetcode/560-subarray-sum-equals-k/subarray-sum-equals-k.py
@@ -2,13 +2,13 @@ class Solution:
     def subarraySum(self, nums: List[int], k: int) -> int:
         count = 0
         prefix_sum = 0
-        indices_sum = {0:1}
+        freq = {0:1}
         for i, num in enumerate(nums):
             prefix_sum += num
             need = prefix_sum - k
-            if need in indices_sum:
-                count += indices_sum[need]
-            indices_sum[prefix_sum] = indices_sum.get(prefix_sum, 0) + 1
-            # print(indices_sum, prefix_sum, count)
+            if need in freq:
+                count += freq[need]
+            freq[prefix_sum] = freq.get(prefix_sum, 0) + 1
+            # print(freq, prefix_sum, count)
         return count
         
